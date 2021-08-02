ff237f6d6e2decc7475251da00bfca2f78c4a16f
Frederic <paulpoiu123@gmail.com>
2021-08-02 23:20:04 +0200

    Csharp_V7_Discard_V1

--- Csharp_V7_Discard/Program.cs
+++ Csharp_V7_Discard/Program.cs
@@ -32,12 +32,62 @@ namespace Csharp_V7_Discard
                 
                 return ("", 0, 0, 0, 0, 0);
             }
+            
+            var p = new Person("John", "Quincy", "Adams", "Boston", "MA");
+            var p2 = new Person("John", "Quincy", "Adams", "Boston", "MA");
+
+            // Deconstruct the person object.
+            var (fName, _, city, _) = p;
+            Console.WriteLine($"Hello {fName} of !");
+            var (_, lname) = p2;
+            Console.WriteLine($"Hello {lname} of !");
             //static is from C#8.0
 
             //reference
             //https://docs.microsoft.com/zh-tw/dotnet/csharp/fundamentals/functional/discards
             //https://docs.microsoft.com/zh-tw/dotnet/csharp/fundamentals/functional/deconstruct#tuple-elements-with-discards
             
+        }
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string MiddleName { get; set; }
+            public string LastName { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+
+            public Person(string fname, string mname, string lname,
+                string cityName, string stateName)
+            {
+                FirstName = fname;
+                MiddleName = mname;
+                LastName = lname;
+                City = cityName;
+                State = stateName;
+            }
+
+            // Return the first and last name.
+            public void Deconstruct(out string fname, out string lname)
+            {
+                fname = FirstName;
+                lname = LastName;
+            }
+
+            public void Deconstruct(out string fname, out string mname, out string lname)
+            {
+                fname = FirstName;
+                mname = MiddleName;
+                lname = LastName;
+            }
+
+            public void Deconstruct(out string fname, out string lname,
+                out string city, out string state)
+            {
+                fname = FirstName;
+                lname = LastName;
+                city = City;
+                state = State;
+            }
         }
     }
 }
