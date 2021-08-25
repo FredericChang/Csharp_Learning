c002b913b9c15aa2d8acd333022f6d85757a2357
Frederic <paulpoiu123@gmail.com>
2021-08-25 22:03:17 +0200

    Delegate Training

---
+++ Csharp_V_AsyncEnumerables/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace Csharp_V_AsyncEnumerables
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            static IEnumerable<int> Range(int start, int count)
+            {
+                for (int i = 0; i < count; i++)
+                    yield return start + i;
+                //此示例顯示使用“yield return”創建 IEnumerable<T>
+                //IEnumerable C# 2.0
+                //.NET Framework 4 System.Threading.Tasks.Task 和 Task<T>
+                //.NET Framework 4.5 和 C# 5，它引入了 async 和 await 
+            }
+            foreach (int item in Range(10, 3))
+                Console.Write(item + " "); // prints 10 11 12
+
+            static async Task PrintAsync(string format, int iteraions, int delayMilliseconds)
+            {
+                Console.WriteLine("goto PrintAsync");
+
+                for (int i = 0; i < iteraions; i++)
+                {
+                    Console.WriteLine("goto PrintAsync_For");
+
+                    await Task.Delay(delayMilliseconds);
+                    Console.WriteLine("delayMilliseconds");
+                }
+            }
+            Console.WriteLine("goto await PrintAsync");
+            await PrintAsync("Iteration {0}", 5, 1_000);
+            //should correct
+            //static async Task Main(string[] args)
+        }
+    }
+}
+
+//reference
+//https://docs.microsoft.com/zh-tw/archive/msdn-magazine/2019/november/csharp-iterating-with-async-enumerables-in-csharp-8
---
+++ Delegate/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Delegate
+{
+
+    delegate void D(int x);
+
+    class C
+    {
+        public static void M1(int i) 
+        {
+            Console.WriteLine("C.M1: " + i);
+        }
+
+        public static void M2(int i) 
+        {
+            Console.WriteLine("C.M2: " + i);
+        }
+
+        public void M3(int i)
+        {
+            Console.WriteLine("C.M3: " + i);
+        }
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            D cd1 = new D(C.M1);
+            cd1(-1);                // call M1
+            D cd2 = new D(C.M2);
+            cd2(-2);                // call M2
+            D cd3 = cd1 + cd2;
+            cd3(10);                // call M1 then M2
+            Console.WriteLine("Here");
+            cd3 += cd1;
+            cd3(20);
+            Console.WriteLine("Here");
+            C c = new C();
+            D cd4 = new D(c.M3);
+            cd3 += cd4;
+            cd3(30);
+            Console.WriteLine("Here");
+            cd3 -= cd1;             // remove last M1
+            cd3(40);                // call M1, M2, then M3
+            Console.WriteLine("Here");
+            cd3 -= cd4;
+            cd3(50);                // call M1 then M2
+            Console.WriteLine("Here");
+            cd3 -= cd2;
+            cd3(60);                // call M1
+            Console.WriteLine("Here");
+
+            cd3 -= cd2;             // impossible removal is benign
+            cd3(60);                // call M1
+            Console.WriteLine("Here");
+
+            cd3 -= cd1;             // invocation list is empty so cd3 is null
+            Console.WriteLine("70");
+
+            cd3(70);                // System.NullReferenceException thrown
+            Console.WriteLine("Here");
+
+            cd3 -= cd1;             // impossible removal is benign
+        }
+    }
+}
