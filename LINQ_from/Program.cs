d1d459f217b3fc5ae9ac68adf40cbeef9c3facc8
Frederic <paulpoiu123@gmail.com>
2021-08-26 21:34:00 +0200

    LINQ_from Learning

---
+++ LINQ_from/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQ_from
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            char[] upperCase = {'A', 'B', 'C'};
+            char[] lowerCase = {'x', 'y', 'z'};
+
+            var joinQuery1 = 
+                from upper in upperCase 
+                from lower in lowerCase 
+                select new {upper, lower};
+
+            var joinQuery2 = 
+                from lower in lowerCase
+                where lower != 'x'
+                from upper in upperCase
+                select new {upper, lower};
+
+            Console.WriteLine("Cross join:");
+
+            foreach (var pair in joinQuery1)
+            {
+                Console.WriteLine("{0} is matched to {1}", pair.lower, pair.upper);   
+            }
+            
+            Console.WriteLine("Filtered non-equijoin:");
+            // Rest the mouse pointer over joinQuery2 to verify its type.
+            foreach (var pair in joinQuery2)
+            {
+                Console.WriteLine("{0} is matched to {1}", pair.lower, pair.upper);
+            }
+
+        }
+    }
+}
