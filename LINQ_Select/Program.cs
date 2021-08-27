4503c757fe7f318f60e5a11662d99e5298ba8e8d
Frederic <paulpoiu123@gmail.com>
2021-08-27 16:14:48 +0200

    LINQ_GROUP

---
+++ LINQ_Select/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LINQ_Select
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public string First { get; set; }
+            public string Last { get; set; }
+            public int ID { get; set; }
+            public List<int> Scores;
+            public ContactInfo GetContactInfo(Program app, int id)
+            {
+                ContactInfo cInfo =
+                    (from ci in app.contactList
+                    where ci.ID == id
+                    select ci)
+                    .FirstOrDefault();
+
+                return cInfo;
+            }
+
+            public override string ToString()
+            {
+                return First + " " + Last + ":" + ID;
+            }
+        }
+
+        public class ContactInfo
+        {
+            public int ID { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }
+            public override string ToString() { return Email + "," + Phone; }
+        }
+
+        public class ScoreInfo
+        {
+            public double Average { get; set; }
+            public int ID { get; set; }
+        }
+
+        // The primary data source
+        List<Student> students = new List<Student>()
+        {
+             new Student {First="Svetlana", Last="Omelchenko", ID=111, Scores= new List<int>() {97, 92, 81, 60}},
+             new Student {First="Claire", Last="O'Donnell", ID=112, Scores= new List<int>() {75, 84, 91, 39}},
+             new Student {First="Sven", Last="Mortensen", ID=113, Scores= new List<int>() {88, 94, 65, 91}},
+             new Student {First="Cesar", Last="Garcia", ID=114, Scores= new List<int>() {97, 89, 85, 82}},
+        };
+
+        // Separate data source for contact info.
+        List<ContactInfo> contactList = new List<ContactInfo>()
+        {
+            new ContactInfo {ID=111, Email="SvetlanO@Contoso.com", Phone="206-555-0108"},
+            new ContactInfo {ID=112, Email="ClaireO@Contoso.com", Phone="206-555-0298"},
+            new ContactInfo {ID=113, Email="SvenMort@Contoso.com", Phone="206-555-1130"},
+            new ContactInfo {ID=114, Email="CesarGar@Contoso.com", Phone="206-555-0521"}
+        };
+        public static void Main(string[] args)
+        {
+            List<int> Scores = new List<int>() { 97, 92, 81, 60 };
+
+            // Create the query.
+            IEnumerable<int> queryHighScores =
+                from score in Scores
+                where score > 90
+                select score;
+
+            // Execute the query.
+            foreach (int i in queryHighScores)
+            {
+                Console.Write(i + ", ");
+            }
+            
+            Program app = new Program();
+            
+            IEnumerable<Student> studentQuery1 =
+                from student in app.students
+                where student.ID > 111
+                select student;
+
+            Console.WriteLine("Query1: select range_variable");
+            foreach (Student s in studentQuery1)
+            {
+                Console.WriteLine(s.ToString());
+            }
+            
+            IEnumerable<string> studentQuery2 =
+                from student in app.students
+                where student.ID > 111
+                select student.Last;
+
+            Console.WriteLine("Query2:");
+            foreach (string s in studentQuery2)
+            {
+                Console.WriteLine(s.ToString());
+            }
+            
+            IEnumerable<ContactInfo> studentQuery3 =
+                from student in app.students
+                where student.ID > 111
+                select student.GetContactInfo(app, student.ID);
+
+            Console.WriteLine("\r\n studentQuery3: select range_variable.Method");
+            foreach (ContactInfo ci in studentQuery3)
+            {
+                Console.WriteLine(ci.ToString());
+            }
+
+            IEnumerable<int> studentQuery4 =
+                from student in app.students
+                where student.ID > 111
+                select student.Scores[0];
+            
+            foreach (int score in studentQuery4)
+            {
+                Console.WriteLine(score);
+            }
+
+            IEnumerable<double> studentQuery5 =
+                from student in app.students
+                where student.ID > 111
+                select student.Scores[0] * 1.1;
+
+            Console.WriteLine("\r\n studentQuery5: select expression");
+            foreach (double d in studentQuery5)
+            {
+                Console.WriteLine("Adjusted first score = {0}", d);
+            }
+
+            // Produce a filtered sequence of doubles that are
+            // the result of a method call.
+            IEnumerable<double> studentQuery6 =
+                from student in app.students
+                where student.ID > 111
+                select student.Scores.Average();
+
+            Console.WriteLine("\r\n studentQuery6: select expression2");
+            foreach (double d in studentQuery6)
+            {
+                Console.WriteLine("Average = {0}", d);
+            }
+
+            // Produce a filtered sequence of anonymous types
+            // that contain only two properties from each Student.
+            var studentQuery7 =
+                from student in app.students
+                where student.ID > 111
+                select new { student.First, student.Last };
+
+            Console.WriteLine("\r\n studentQuery7: select new anonymous type");
+            foreach (var item in studentQuery7)
+            {
+                Console.WriteLine("{0}, {1}", item.Last, item.First);
+            }
+
+            // Produce a filtered sequence of named objects that contain
+            // a method return value and a property from each Student.
+            // Use named types if you need to pass the query variable
+            // across a method boundary.
+            IEnumerable<ScoreInfo> studentQuery8 =
+                from student in app.students
+                where student.ID > 111
+                select new ScoreInfo
+                {
+                    Average = student.Scores.Average(),
+                    ID = student.ID
+                };
+
+            Console.WriteLine("\r\n studentQuery8: select new named type");
+            foreach (ScoreInfo si in studentQuery8)
+            {
+                Console.WriteLine("ID = {0}, Average = {1}", si.ID, si.Average);
+            }
+
+            // Produce a filtered sequence of students who appear on a contact list
+            // and whose average is greater than 85.
+            IEnumerable<ContactInfo> studentQuery9 =
+                from student in app.students
+                where student.Scores.Average() > 85
+                join ci in app.contactList on student.ID equals ci.ID
+                select ci;
+
+            Console.WriteLine("\r\n studentQuery9: select result of join clause");
+            foreach (ContactInfo ci in studentQuery9)
+            {
+                Console.WriteLine("ID = {0}, Email = {1}", ci.ID, ci.Email);
+            }
+
+        }
+    }
+}
