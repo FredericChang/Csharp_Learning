0cd7c7e6c8728441784a839c22aca0dd28d10760
Frederic <paulpoiu123@gmail.com>
2021-12-23 20:57:42 +0100

    Added one more folder with JSON

--- MobilePhoneOOP/Mobile.cs
+++ MobilePhoneOOP/Mobile.cs
@@ -1,35 +1,38 @@
 ﻿using System;
 using MobilePhoneOOP.Interface;
 
+///https://www.c-sharpcorner.com/UploadFile/cda5ba/object-oriented-programming-with-real-world-scenario/
+/// Reference
+/// https://www.codeproject.com/Articles/22769/Introduction-to-Object-Oriented-Programming-Concep
 namespace MobilePhoneOOP
 {
     public class Mobile : ITelephone, IMobile
     {
-    private string IEMIcode { get; set; }
-    public string SIMCard { get; set; }
-    public string Processor { get; }
-    public int InternalMemory { get; }
-    public bool IsSignalSIM { get; set; }
+        private string IEMIcode { get; set; }
+        public string SIMCard { get; set; }
+        public string Processor { get; }
+        public int InternalMemory { get; }
+        public bool IsSignalSIM { get; set; }
 
-    public void GetIEMIcode()
-    {
-        Console.WriteLine("IEMI Code - IEDF3434");
-    }
+        public void GetIEMIcode()
+        {
+            Console.WriteLine("IEMI Code - IEDF3434");
+        }
 
 
-    public void Receive()
-    {
-        Console.WriteLine();
-    }
+        public void Receive()
+        {
+            Console.WriteLine();
+        }
 
-    public virtual void SendMessage()
-    {
-        Console.WriteLine();
-    }
+        public virtual void SendMessage()
+        {
+            Console.WriteLine();
+        }
 
-    public void Dial()
-    {
-        throw new NotImplementedException();
-    }
+        public void Dial()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MobilePhoneOOP/Program.cs
+++ MobilePhoneOOP/Program.cs
@@ -1,11 +1,19 @@
-﻿namespace MobilePhoneOOP
+﻿using System.Collections.Generic;
+using MobilePhoneOOP.Interface;
+
+namespace MobilePhoneOOP
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
-            
+            List<IMobile> Dial = new List<IMobile>();
+            Dial.Add(new Nokia());
+            Dial.Add(new Samsung());
+            foreach (var o in  Dial)
+            {
+                o.Dial();
+            }
         }
     }
 }
