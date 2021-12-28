09e766b3cd5a6c463996c3f3c53934383f6f3550
Frederic <paulpoiu123@gmail.com>
2021-12-28 13:12:35 +0100

    Thread_Background Added

---
+++ Thread_Background/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace Thread_Background
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            // FileStream fs = new FileStream("Test.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+            // // First, save the standard output.
+            // TextWriter tmp = Console.Out;
+            // StreamWriter sw = new StreamWriter(fs);
+            // SetOut(sw);
+            // Console.WriteLine("Hello file");
+            // Console.SetOut(tmp);
+            // Console.WriteLine("Hello World");
+            
+            var sampleForeground = new ThreadSample(10);
+            var sampleBackground = new ThreadSample(20);
+
+            var threadOne = new Thread(sampleForeground.CountNumbers);
+            threadOne.Name = "ForegroundThread";
+            var threadTwo = new Thread(sampleBackground.CountNumbers);
+            threadTwo.Name = "BackgroundThread";
+            threadTwo.IsBackground = true;
+            
+            threadOne.Start();
+            threadTwo.Start();
+            // sw.Close();
+        }
+
+        class ThreadSample
+        {
+            private readonly int _iterations;
+
+            public ThreadSample(int iterations)
+            {
+                _iterations = iterations;
+            }
+
+            public void CountNumbers()
+            {
+                for (int i = 0; i < _iterations; i++)
+                {
+                    Sleep(500);
+                    WriteLine($"{CurrentThread.Name} prints {i}");
+                }
+            }
+        }
+    }
+}
