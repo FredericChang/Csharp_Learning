ddebfdfb1ae2576a815c98b35180b05bc767a844
Frederic <paulpoiu123@gmail.com>
2022-01-03 23:59:03 +0800

    Modification.

---
+++ random_Double/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace random_Double
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ random_Double2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace random_Double2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random a = new Random(1);
+            double aa = 0;
+            aa = NextDouble(a, 1, 5);
+            Console.WriteLine(aa);
+            var rand = new Random();
+
+            for (int ctr = 0; ctr <= 4; ctr++)
+            {
+                
+                Console.Write("{0,10:N4}", rand.NextDouble() * 5);
+            }
+                
+        }
+        
+        public static double NextDouble(Random RandGenerator, double MinValue, double MaxValue)
+        {
+            return RandGenerator.NextDouble() * (MaxValue - MinValue) + MinValue;
+        }
+
+    }
+}
