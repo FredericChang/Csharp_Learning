44109cf41021ad95e0057a800b3e0a8947dd40a3
Frederic <paulpoiu123@gmail.com>
2022-02-02 23:34:13 +0100

    Added new project

---
+++ OOD_CleanCode_Ch9/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OOD_CleanCode_Ch9
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            
+        }
+        public void voidDrawAllShapes(Ilist shapes)
+        {
+            foreach (Shape shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+
+    }
+    
+
+    public interface Shape
+    {
+        void Draw();
+    }
+
+    public class Square : Shape
+    {
+        public void Draw()
+        {
+            
+        }
+    }
+    
+    public class Circle : Shape
+    {
+        public void Draw()
+        {
+            
+        }
+    }
+    
+     
+}
