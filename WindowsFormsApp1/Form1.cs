4fd5f1d5a8dfb7bda918e5e87f6ea6b7c39027d7
Frederic <paulpoiu123@gmail.com>
2022-07-11 07:23:37 +0200

    Updated.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+        static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+
+        static readonly IntPtr HWND_TOP = new IntPtr(0);
+
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+
+        const UInt32 SWP_NOSIZE = 0x0001;
+
+        const UInt32 SWP_NOMOVE = 0x0002;
+
+        const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        
+        [DllImport("user32.dll")]
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopLevel = true;
+            this.TopMost = true;
+            this.MinimizeBox = false;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
---
+++ _0013_Roman/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace _0013_Roman
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            
+        }
+
+        public int RomanTOInt(string s)
+        {
+            int sum = 0;
+
+            Dictionary<char, int> romanNumber = new()
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
+            };
+
+            for ( int i = 0; i < s.Length; i++)
+            {
+                char currentRomanChar = s[i];
+                romanNumber.TryGetValue(currentRomanChar, out int num);
+                if (i + 1 < s.Length && romanNumber[s[i + 1]] > romanNumber[currentRomanChar])
+                {
+                    sum -= num;
+                }
+                else
+                {
+                    sum += num;
+                }
+            }
+            
+            return sum;
+        }
+    }
+}
